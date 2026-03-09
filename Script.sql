10e2e058c15b6f595c3d70ead8dbb7199bfd9222
Henrique Teodoro Silva <henriqueads417@gmail.com>
2026-03-09 20:28:22 -0300

    Ex5

---
+++ Script.sql
@@ -0,0 +1,32 @@
+SELECT COUNT(*) FROM advisor;
+--nº de registros: 2000
+
+SELECT COUNT(*) FROM classroom;
+--nº de registros: 30
+
+SELECT COUNT(*) FROM course;
+--nº de registros: 200
+
+SELECT COUNT(*) FROM department;
+--nº de registros: 20
+
+SELECT COUNT(*) FROM instructor;
+--nº de registros: 50
+
+SELECT COUNT(*) FROM prereq;
+--nº de registros: 100
+
+SELECT COUNT(*) FROM section;
+--nº de registros: 100
+
+SELECT COUNT(*) FROM student;
+--nº de registros: 2000
+
+SELECT COUNT(*) FROM takes;
+--nº de registros: 30000
+
+SELECT COUNT(*) FROM teaches;
+--nº de registros: 100
+
+SELECT COUNT(*) FROM time_slot;
+--nº de registros: 20
